e0aa0aabdbebcee04479cac98679d238f3b69f57
Daniel <angelicodaniel93@gmail.com>
2023-06-22 16:12:32 +0200

    images

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,6 +1,10 @@
 INSERT INTO pizzas (name, description, image, price)
 VALUES
   ('Margherita', 'Classic pizza with tomato sauce, mozzarella, and basil', 'margherita.jpg', 9.99),
-  ('Pepperoni', 'Pizza topped with pepperoni slices and mozzarella cheese', 'pepperoni.jpg', 11.99),
+  ('Diavola', 'Spicy pizza with tomato sauce, mozzarella, and spicy salami', 'diavola.jpg', 10.99),
   ('Vegetariana', 'Pizza loaded with assorted vegetables and cheese', 'vegetariana.jpg', 10.99),
-  ('Quattro Formaggi', 'Pizza topped with four different types of cheese', 'quattro_formaggi.jpg', 12.99);
+  ('Quattro Formaggi', 'Pizza topped with four different types of cheese', 'quattro_formaggi.jpg', 12.99),
+  ('Pizza al Pistacchio', 'Pizza with creamy pistachio sauce and mozzarella', 'pistacchio.jpg', 11.99),
+  ('Capricciosa', 'Pizza with tomato sauce, mozzarella, ham, mushrooms, artichokes, olives, and oregano', 'capricciosa.jpg', 12.99),
+  ('Bufalina', 'Pizza with tomato sauce, buffalo mozzarella, and fresh basil', 'bufalina.jpg', 11.99),
+  ('Prosciutto e Funghi', 'Pizza with tomato sauce, mozzarella, ham, and mushrooms', 'prosciutto_funghi.jpg', 10.99);
