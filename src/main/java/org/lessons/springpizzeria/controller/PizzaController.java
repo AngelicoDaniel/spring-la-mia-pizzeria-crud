d6d5dc25edf49355418a21bcfdb074b777e7e373
Daniel <angelicodaniel93@gmail.com>
2023-06-23 16:12:13 +0200

    show + searchbar

--- src/main/java/org/lessons/springpizzeria/controller/PizzaController.java
+++ src/main/java/org/lessons/springpizzeria/controller/PizzaController.java
@@ -3,12 +3,17 @@ package org.lessons.springpizzeria.controller;
 import org.lessons.springpizzeria.model.Pizza;
 import org.lessons.springpizzeria.repository.PizzaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/pizzas")
@@ -17,11 +22,27 @@ public class PizzaController {
     @Autowired
     private PizzaRepository pizzaRepository;
 
-    @GetMapping
-    public String list(Model model){
-        List<Pizza> pizzas = pizzaRepository.findAll();
-        model.addAttribute("pizzaIndex", pizzas);
-        return "/pizzas/index";
+    @GetMapping("/{id}")
+    public String detail(@PathVariable("id") Integer pizzaId, Model model) {
+        Optional<Pizza> result = pizzaRepository.findById(pizzaId);
+        if (result.isPresent()) {
+            model.addAttribute("pizza", result.get());
+            return "/pizzas/detail";
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Pizza with id " + pizzaId + " not found");
+        }
     }
 
+    @GetMapping
+    public String list(@RequestParam(name = "keyword", required = false) String searchString, Model model) {
+        List<Pizza> pizzas;
+        if (searchString == null || searchString.isBlank()) {
+            pizzas = pizzaRepository.findAll();
+        } else {
+            pizzas = pizzaRepository.findByNameContainingIgnoreCase(searchString);
+        }
+        model.addAttribute("pizzaList", pizzas);
+        model.addAttribute("searchInput", searchString);
+        return "/pizzas/index";
+    }
 }
